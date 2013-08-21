d995c428e5b508d474091a26bd7e618b6b0b896a
Robert Landers <landers.robert@gmail.com>
2013-08-20 21:47:34 -0400

    Updated all libraries, added storage for stuff

---
+++ api/Models/AmazonProduct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using Microsoft.WindowsAzure;
+
+namespace api.Models
+{
+    public class AmazonProduct : TableEntity
+    {
+        public AmazonProduct(string code, string asin)
+        {
+            this.PartitionKey = asin;
+            this.RowKey = code;
+        }
+
+        public AmazonProduct()
+        {
+        }
+
+        public string Code { get; set; }
+        public string ASIN { get; set; }
+    }
+}
---
+++ api/Models/TProduct.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using Microsoft.WindowsAzure;
+using MarketplaceWebServiceProducts;
+
+namespace api.Models
+{
+    public class TProduct
+    {
+        public string ProductCode
+        {
+            get;
+            set;
+        }
+
+        public int Qty
+        {
+            get;
+            set;
+        }
+
+        private CloudStorageAccount cloud;
+        private CloudTableClient client;
+        private CloudTable products;
+
+        public TProduct(string code, int qty)
+        {
+            ProductCode = code;
+            Qty = qty;
+
+            cloud = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("AbundaStorage"));
+
+            client = cloud.CreateCloudTableClient();
+
+            products = client.GetTableReference("Products");
+            products.CreateIfNotExists();
+        }
+
+        public void PerformAmazonLookup()
+        {
+
+        }
+    }
+}
